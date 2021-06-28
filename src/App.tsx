6d410974714c141b525ec5e56efb9e4595066411
Bonnie Simon <bonnie.developer@gmail.com>
2021-06-28 13:59:50 +0530

    Tailwind worked. Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+// Core Libraries
 import React from 'react';
-import './App.css';
+// Components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Preview-mark</h1> 
+    <div className="App min-h-screen bg-gray-700">
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+const Navbar = () => {
+	return (
+		<header className="text-gray-100 body-font">
+			<div
+				className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+			>
+				<a
+					className="flex title-font font-medium items-center text-gray-50 mb-4 md:mb-0"
+				>
+					<img src="../assets/img/logo.png" alt="" className="w-8" />
+					<span className="ml-3 text-2xl">PreviewMark</span>
+				</a>
+				<nav
+					className="md:ml-auto flex flex-wrap items-center text-base justify-center"
+				>
+					<ul className="flex flex-col lg:flex-row list-none ml-auto">
+						<li className="nav-item">
+							<a
+								className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+								href="#"
+							>
+								<i className="fab fa-facebook-square text-lg leading-lg opacity-75">
+								</i>
+								<span className="ml-2">Share</span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a
+								className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+								href="#"
+							>
+								<i className="fab fa-twitter text-lg leading-lg opacity-75"> </i>
+								<span className="ml-2">Tweet</span>
+							</a>
+						</li>
+
+						<li className="nav-item">
+							<a
+								className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+								href="#"
+							>
+								<i className="fab fa-github text-lg leading-lg opacity-75"> </i>
+								<span className="ml-2">Pin</span>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	)
+}
+
+export default Navbar
