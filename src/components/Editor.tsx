8af60077dadf3682f5a872e85ab5d003b8b4f964
Bonnie Simon <bonnie.developer@gmail.com>
2021-07-29 17:35:35 +0530

    Changed text color of Editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -8,7 +8,7 @@ const Editor: FC = () => {
 		<div className="w-full mx-5">
 			<label className="block text-gray-300 text-center font-bold my-10" htmlFor="message">Enter Markdown</label>
 			<textarea value={editorMarkdown} onChange={setEditorMarkdownHandle} name="feedback" id="feedback"
-					className="border-0 px-3 py-3 bg-gray-600 placeholder-black text-gray-800 rounded shadow focus:outline-none w-full h-96"
+					className="border-0 px-3 py-3 bg-gray-600 placeholder-black text-gray-50 rounded shadow focus:outline-none w-full h-96"
 					placeholder="" required />
 		</div>
 	)
