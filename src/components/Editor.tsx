ca3efdee15a1f8f4cc0029a7fd4a2807a413b048
Bonnie Simon <bonnie.developer@gmail.com>
2021-07-29 17:31:40 +0530

    Added state and onChange to textarea in editor; editorMarkdown and setEditorMarkdownHandler

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,14 +3,13 @@ import { FC, useContext } from "react"
 import { MarkdownContext } from "../context/markdownContext";
 
 const Editor: FC = () => {
-	const {setMarkdownHandle} = useContext(MarkdownContext);
+	const {setEditorMarkdownHandle, editorMarkdown} = useContext(MarkdownContext);
 	return (
 		<div className="w-full mx-5">
 			<label className="block text-gray-300 text-center font-bold my-10" htmlFor="message">Enter Markdown</label>
-			<textarea  name="feedback" id="feedback"
+			<textarea value={editorMarkdown} onChange={setEditorMarkdownHandle} name="feedback" id="feedback"
 					className="border-0 px-3 py-3 bg-gray-600 placeholder-black text-gray-800 rounded shadow focus:outline-none w-full h-96"
-					placeholder="" required>
-			</textarea>
+					placeholder="" required />
 		</div>
 	)
 }
