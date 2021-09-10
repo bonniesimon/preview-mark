6a7085f5d0fc7bdbab5100bf47fbd913c1906835
Bonnie Simon <bonnie.developer@gmail.com>
2021-09-10 18:47:33 +0530

    Add renderer for marked to a new file in /utils
    To clean up the code in markdownContext, moving the renderer object to
    the /utils file and importing it into the markdownContext file.

--- src/context/markdownContext.tsx
+++ src/context/markdownContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, useState } from "react";
 import marked, { MarkedExtension } from 'marked';
+// Helper Functions
+import {renderer} from './../utils/markedUtils';
 
 type Props ={
 	children:React.ReactNode
@@ -33,44 +35,6 @@ const MarkdownProvider = ({children}: any): any => {
 	
 	const setPreviewMarkdownHandle = (markdown: string) => {
 		// let modifiedStringWithBrTag = markdown.replace(/\n/g, "<br>");
-		const renderer:any= {
-			heading(text: string, level: number){
-				const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
-				let headingSizeTailwind: string = 'base';
-				const hrTag: string = level === 1 ? '<hr class="p-3"/>' : '';
-				switch(level){
-					case 1:
-						headingSizeTailwind = '3xl py-2'
-						break;
-						case 2:
-							headingSizeTailwind = '2xl';
-							break;
-							case 3:
-								headingSizeTailwind = 'xl';
-						break;
-						case 4:
-							headingSizeTailwind = 'lg';
-							break;
-							case 5:
-								headingSizeTailwind = 'base';
-								break;
-							}	
-							return `
-					<h${level} class="text-${headingSizeTailwind}">
-					${text}
-					</h${level}>
-					${hrTag}
-					`;
-				},
-			list(body: string, ordered: boolean){
-				const listStyleTypeTailwind: string = ordered? 'decimal': 'disc';
-				return `
-				<ul class="list-${listStyleTypeTailwind} px-4">
-				${body}
-				</ul>
-				`
-			}
-		};
 
 		// Saving item to localStorage.
 		localStorage.setItem('editorMarkdown', markdown);
---
+++ src/utils/markedUtils.ts
@@ -0,0 +1,40 @@
+const renderer:any= {
+	heading(text: string, level: number){
+		const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+		let headingSizeTailwind: string = 'base';
+		const hrTag: string = level === 1 ? '<hr class="p-3"/>' : '';
+		switch(level){
+			case 1:
+				headingSizeTailwind = '3xl py-2'
+				break;
+				case 2:
+					headingSizeTailwind = '2xl';
+					break;
+					case 3:
+						headingSizeTailwind = 'xl';
+				break;
+				case 4:
+					headingSizeTailwind = 'lg';
+					break;
+					case 5:
+						headingSizeTailwind = 'base';
+						break;
+					}	
+					return `
+			<h${level} class="text-${headingSizeTailwind}">
+			${text}
+			</h${level}>
+			${hrTag}
+			`;
+		},
+	list(body: string, ordered: boolean){
+		const listStyleTypeTailwind: string = ordered? 'decimal': 'disc';
+		return `
+		<ul class="list-${listStyleTypeTailwind} px-4">
+		${body}
+		</ul>
+		`
+	}
+};
+
+export {renderer};
