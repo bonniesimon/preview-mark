3015398e225c4d124f43823a00d2635347a6f8b2
Bonnie Simon <bonnie.developer@gmail.com>
2021-09-11 15:16:49 +0530

    Remove self-center class to avoid the Editor from shrinking.

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,7 +5,7 @@ import { MarkdownContext } from "../context/markdownContext";
 const Editor: FC = () => {
 	const {setEditorMarkdownHandle, editorMarkdown} = useContext(MarkdownContext);
 	return (
-		<div className="self-center min-h-screen lg:w-full md:w-3/4 mx-5">
+		<div className="min-h-screen lg:w-full md:w-3/4 mx-5">
 			<label className="block text-gray-300 text-center font-bold my-10" htmlFor="message">Enter Markdown</label>
 			<textarea value={editorMarkdown} onChange={setEditorMarkdownHandle} name="feedback" id="feedback"
 					className="border-0 px-3 py-3 w-full  h-96 bg-gray-600 placeholder-black text-gray-50 rounded shadow focus:outline-none"
