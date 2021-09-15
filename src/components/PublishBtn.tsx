cbb39bee983b9795d4b1c19257fad7bccbc5409e
Bonnie Simon <bonnie.developer@gmail.com>
2021-09-15 12:15:20 +0530

    Create PublishBtn component

---
+++ src/components/PublishBtn.tsx
@@ -0,0 +1,10 @@
+const PublishBtn = () => {
+	return (
+		<button className="px-3 py-2 bg-yellow-400 text-black flex flex-row justify-center items-center">
+			Publish
+			<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" /></svg>
+		</button>
+	)
+}
+
+export default PublishBtn
