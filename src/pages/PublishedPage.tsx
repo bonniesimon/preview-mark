72016a3bcb46fc9d4cafeb124652e5194690d8c2
Bonnie Simon <bonnie.developer@gmail.com>
2021-09-16 18:52:37 +0530

    Implement logic for fetching page content using page id
    For now I've only placed the htmlcontent in state. Need to see if I need
    any other data.

--- src/pages/PublishedPage.tsx
+++ src/pages/PublishedPage.tsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react"
+
 const PublishedPage = ({match}: any) => {
-	// console.log(params);
+	const [htmlContent, setHtmlContent] = useState('');
+
+	const fetchPage = async (apiUrl: string) => {
+		const res = await fetch(apiUrl);
+		const json = await res.json();
+		setHtmlContent(json.markdown);
+	}
+	useEffect(() => {
+		const pageId:string = match.params.id;
+		const apiUrl: string = `https://f80l28.deta.dev/pages/${pageId}`;
+		fetchPage(apiUrl);
+	}, [])
 	return (
 		<div>
 			<h2>Welcome to publish page</h2>	
