d7adf868d1c3bde7aba1258df04a457a176ea6ac
Alec Trievel <atrievel@gmail.com>
2021-10-04 14:25:43 -0400

    Add tooltip to publish button

--- src/components/PublishBtn.tsx
+++ src/components/PublishBtn.tsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { Redirect, useHistory } from "react-router";
 import { MarkdownContext } from "../context/markdownContext";
+import ReactTooltip from "react-tooltip";
 
 // interface IApiOptions{
 // 	method: string;
@@ -40,10 +41,14 @@ const PublishBtn = () => {
 		history.push(redirectUrl);
 	}
 	return (
-		<button onClick={handlePublishClick} className="px-3 py-2 bg-yellow-400 text-black flex flex-row justify-center items-center">
-			Publish
-			<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" /></svg>
-		</button>
+		<React.Fragment>
+			<button onClick={handlePublishClick} className="px-3 py-2 bg-yellow-400 text-black flex flex-row justify-center items-center" 
+				data-tip="Click to publish your markdown" data-place="bottom">
+				Publish
+				<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" /></svg>
+			</button>
+			<ReactTooltip />
+		</React.Fragment>
 	)
 };
 
